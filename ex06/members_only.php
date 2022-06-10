ae5c825c1396b5cb478c25af49583f34678b426d
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-06-10 22:28:43 +0300

    ex06, members_only.php

---
+++ ex06/members_only.php
@@ -0,0 +1,15 @@
+<?php
+	if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "Ilovemylittleponey")
+	{
+		$img = file_get_contents("../img/42.png");
+		echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,";
+		echo base64_encode($img);
+		echo "'>\n</body></html>\n";
+	}
+	else
+	{
+		header('HTTP/1.0 401 Unauthorized');
+		header('WWW-Authenticate: Basic realm=\'\'Member area\'\'');
+		echo "<html><body>That area is accessible for members only</body></html>     \n";
+	}
+?>
